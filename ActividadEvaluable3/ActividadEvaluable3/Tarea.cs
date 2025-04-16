b902e52e4093fe59a9c91ccd8f2fb9a85d509717
Dieote <gomezmdiego97@gmail.com>
2025-04-16 19:36:51 +0200

    ID
    Metodo para enuimerar tareas

--- ActividadEvaluable3/ActividadEvaluable3/Tarea.cs
+++ ActividadEvaluable3/ActividadEvaluable3/Tarea.cs
@@ -4,22 +4,24 @@ public class Tarea
     public string Descripcion { get; set; }
     public bool Prioridad { get; set; }
     public TipoTarea TipoTarea { get; set; }
-
+    public int IdTarea { get; set; }
+    private static int contadorId = 1;
+    
     public Tarea(string nombre, string descripcion, TipoTarea tipoTarea, bool prioridad){
-    Nombre = nombre;
-    Descripcion = descripcion;
-    TipoTarea = tipoTarea;
-    Prioridad = prioridad;
+        IdTarea = contadorId++;
+        Nombre = nombre;
+        Descripcion = descripcion;
+        TipoTarea = tipoTarea;
+        Prioridad = prioridad;
 }
-
 public void MostrarTarea(){
-    Console.WriteLine($"**TAREA | Tipo: {TipoTarea}, Nombre: {Nombre} Prioridad: {Prioridad} Descripcion: {Descripcion}");
+    Console.WriteLine($"**TAREA {IdTarea} | Tipo: {TipoTarea}, Nombre: {Nombre} Prioridad: {Prioridad} Descripcion: {Descripcion}");
 }
 
 public static void MostrarListadoTareas(List<Tarea> tareas){
             Console.WriteLine($"** Lista tareas **");
-            foreach (var Tarea in tareas){
-            Console.WriteLine($"ID | Nombre {Tarea.Nombre} | Descripcion {Tarea.Descripcion} | Prioridad {Tarea.Prioridad} | Tipo {Tarea.TipoTarea}");
+            foreach (var tarea in tareas){
+            Console.WriteLine($"{tarea.IdTarea} | Nombre {tarea.Nombre} | Descripcion {tarea.Descripcion} | Prioridad {tarea.Prioridad} | Tipo {tarea.TipoTarea}");
     }
     Console.WriteLine($"** --- Fin de la lista --- **");
 }
